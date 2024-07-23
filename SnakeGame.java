ff6460a3c86c40cc8d20bfbcda9f306dee2f82f7
Vamilmohan <143425320+Vamilmohan@users.noreply.github.com>
2024-07-23 20:04:21 +0530

    Add files via upload

---
+++ SnakeGame.java
@@ -0,0 +1,25 @@
+package src;
+import javax.swing.JFrame;
+
+public class SnakeGame extends JFrame {
+
+    public SnakeGame() {
+        initUI();
+    }
+
+    private void initUI() {
+        add(new GameBoard());
+        setResizable(false);
+        pack();
+        setTitle("Snake Game");
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    public static void main(String[] args) {
+        java.awt.EventQueue.invokeLater(() -> {
+            JFrame ex = new SnakeGame();
+            ex.setVisible(true);
+        });
+    }
+}
